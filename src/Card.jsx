73f03856775a2ded48151461d50b66f5009e8af9
Schlumen <lumen.haendler@web.de>
2024-04-24 16:09:52 +0200

    Add functionality

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import Title from "./Title";
 import Count from "./Count";
 import ResetButton from "./ResetButton";
 import CountButtons from "./CountButtons";
 
 export default function Card() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="card">
       <Title />
-      <Count />
-      <ResetButton />
-      <CountButtons />
+      <Count count={count} />
+      <ResetButton setCount={setCount} />
+      <CountButtons setCount={setCount} />
     </div>
   );
 }
